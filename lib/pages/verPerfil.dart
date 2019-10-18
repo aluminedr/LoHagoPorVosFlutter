992220cf0ba5ee2c5739b3c22249bd4ad72921eb
ale <alejandrowasilewski@gmail.com>
2019-10-18 18:35:30 -0300

    modificacion de datos
    falta modificar password y email

--- lib/pages/verPerfil.dart
+++ lib/pages/verPerfil.dart
@@ -1,7 +1,9 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
 
-import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:flutter_app/api/api.dart';
+import 'package:flutter_app/pages/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 
 
@@ -13,11 +15,19 @@ class VerPerfilPage extends StatefulWidget{
   }
   
   var _scaffoldKey = new GlobalKey<ScaffoldState>();
-  String idUsuario;
-  //String idPersona;
-   
-
-
+  String nombrePersona;
+  String apellidoPersona;
+  String dniPersona;
+  String telefonoPersona;
+  String imagenPersona;
+  String mailUsuario;
+  String nombreUsuario;
+  String claveUsuario;
+  int idUsuario;
+  var valor;
+  var nuevoValor;
+  var nuevoMail;
+  var clave;
 
  ScaffoldState scaffoldState;
   _mostrarMensaje(msg) async {
@@ -36,6 +46,41 @@ class VerPerfilPage extends StatefulWidget{
 
 
   class _VerPerfilPageState extends State<VerPerfilPage>{
+    
+    @override
+    void initState() {
+      getDetalles();
+      super.initState();
+    }
+
+
+    void getDetalles() async {
+    SharedPreferences localStorage = await SharedPreferences.getInstance();
+    var idPersona =  localStorage.getInt('idPersona');
+    var usuarioJson = localStorage.getString('user');
+    var usuario = json.decode(usuarioJson);
+    mailUsuario= usuario['mailUsuario'];
+    nombreUsuario= usuario['nombreUsuario'];
+    idUsuario= usuario['idUsuario'];
+    var data ={
+      'idPersona': idPersona,
+    };
+    var res = await CallApi().postData(data,'perfil');
+    var persona = json.decode(res.body);
+
+    setState(() {
+      nombrePersona = persona['persona']['nombrePersona'];
+      apellidoPersona = persona['persona']['apellidoPersona'];
+      dniPersona = persona['persona']['dniPersona'];
+      telefonoPersona = persona['persona']['telefonoPersona'];
+      imagenPersona = persona['persona']['imagenPersona'];
+    });
+    
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -47,9 +92,7 @@ class VerPerfilPage extends StatefulWidget{
                       icon: Icon(
                         Icons.settings,
                         color: Colors.white,
-                      ), onPressed: () {
-                         _verPersona();
-                      },
+                      ), onPressed: () {},
                     )]
       ),
         backgroundColor: Color.fromRGBO(255, 255, 255, .9),
@@ -79,7 +122,7 @@ class VerPerfilPage extends StatefulWidget{
                         padding: EdgeInsets.all(4),
                       ),
                       Text(
-                        "Sudip Thapa",
+                        "$nombrePersona",
                         style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.w500,
@@ -90,7 +133,7 @@ class VerPerfilPage extends StatefulWidget{
                         padding: EdgeInsets.all(4),
                       ),
                       Text(
-                        "Kathmandu",
+                        "$apellidoPersona",
                         style: TextStyle(
                             color: Colors.white,
                             fontWeight: FontWeight.w400,
@@ -183,7 +226,7 @@ class UserInfo extends StatelessWidget {
                   Container(
                     alignment: Alignment.topLeft,
                     child: Text(
-                      "User Information",
+                      "Mis datos",
                       style: TextStyle(
                         color: Colors.black87,
                         fontWeight: FontWeight.w500,
@@ -197,32 +240,94 @@ class UserInfo extends StatelessWidget {
                   ),
                   Container(
                       child: Column(
-                    children: <Widget>[
-                      ListTile(
-                        contentPadding:
-                            EdgeInsets.symmetric(horizontal: 12, vertical: 4),
-                        leading: Icon(Icons.my_location),
-                        title: Text("Location"),
-                        subtitle: Text("Kathmandu"),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.email),
-                        title: Text("Email"),
-                        subtitle: Text("sudeptech@gmail.com"),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.phone),
-                        title: Text("Phone"),
-                        subtitle: Text("99--99876-56"),
-                      ),
-                      ListTile(
-                        leading: Icon(Icons.person),
-                        title: Text("About Me"),
-                        subtitle: Text(
-                            "This is a about me link and you can khow about me in this section."),
-                      ),
-                    ],
-                  ))
+                      children: <Widget>[
+                        FlatButton(
+                          textColor: Colors.black,
+                          child: ListTile(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                                  leading: Icon(Icons.person),
+                                  title: Text("NOMBRE"),
+                                  subtitle: Text("$nombrePersona"),
+                                ),
+                          onPressed: (){
+                            showAlertDialog(context,nombrePersona,'Nombre','nombrePersona');
+                          },
+                        ),
+                      ])
+                  ),
+                  Container(
+                      child: Column(
+                      children: <Widget>[
+                        FlatButton(
+                          textColor: Colors.black,
+                          child: ListTile(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                                  leading: Icon(Icons.person),
+                                  title: Text("APELLIDO"),
+                                  subtitle: Text("$apellidoPersona"),
+                                ),
+                          onPressed: (){
+                            showAlertDialog(context,apellidoPersona,'Apellido','apellidoPersona');
+                          },
+                        ),
+                      ])
+                   ),
+                  Container(
+                      child: Column(
+                      children: <Widget>[
+                        FlatButton(
+                          textColor: Colors.black,
+                          child: ListTile(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                                  leading: Icon(Icons.email),
+                                  title: Text("MAIL"),
+                                  subtitle: Text("$mailUsuario"),
+                                ),
+                          onPressed: (){
+                            showAlertDialogMail(context,mailUsuario,'Mail','mailUsuario');
+                          },
+                        ),
+                      ])
+                   ),
+                  Container(
+                      child: Column(
+                      children: <Widget>[
+                        FlatButton(
+                          textColor: Colors.black,
+                          child: ListTile(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                                  leading: Icon(Icons.phone),
+                                  title: Text("Telefono"),
+                                  subtitle: Text("$telefonoPersona"),
+                                ),
+                          onPressed: (){
+                            showAlertDialog(context,telefonoPersona,'Telefono','telefonoPersona');
+                          },
+                        ),
+                      ])
+                   ),
+                   Container(
+                      child: Column(
+                      children: <Widget>[
+                        FlatButton(
+                          textColor: Colors.black,
+                          child: ListTile(
+                                  contentPadding:
+                                      EdgeInsets.symmetric(horizontal: 4, vertical: 4),
+                                  leading: Icon(Icons.vpn_key),
+                                  title: Text("CONTRASEÑA"),
+                                  subtitle: Text("$claveUsuario"),
+                                ),
+                          onPressed: (){
+                            showAlertDialog(context,claveUsuario,'Contraseña','claveUsuario');
+                          },
+                        ),
+                      ])
+                   ),
                 ],
               ),
             ),
@@ -233,12 +338,165 @@ class UserInfo extends StatelessWidget {
   }
 }
 
+  void _actualizar(nuevoValor,columna) async {
+    if (nuevoValor != null && nuevoValor!=''){
+      SharedPreferences localStorage = await SharedPreferences.getInstance();
+      var idPersona =  localStorage.getInt('idPersona');
+      var data = {
+          'idPersona' : idPersona,
+          columna : nuevoValor,
+      };
 
+      var res = await CallApi().postData(data, 'actualizarPerfil');
+      var body = json.decode(res.body);
+
+      nuevoValor = null;
+      if(body['success']){
+        _mostrarMensaje(body['mensaje']);
+      }else{
+        _mostrarMensaje(body['mensaje']);
+      }
+      
+    }
+  }
+
+  void _actualizarMail(nuevoMail,clave) async {
+
+    if (nuevoMail != null && nuevoMail != '' && clave != null && clave != ''){
+      var data = {
+          'idUsuario' : idUsuario,
+          'mailUsuario' : nuevoMail,
+          'clave' : clave
+      };
+      print(data);
+      var res = await CallApi().postData(data, 'actualizarMail');
+      var body = json.decode(res.body);
+      print(body);
+      nuevoMail = null;
+      clave = null;
+
+      if(body['success']){
+        _mostrarMensaje(body['mensaje']);
+      }else{
+        _mostrarMensaje(body['mensaje']);
+      }
+      
+    }
+  }
     
-void _verPersona() async {
+showAlertDialog(BuildContext context,valor,subTitulo,columna) {
 
-  SharedPreferences localStorage = await SharedPreferences.getInstance();
+  // set up the buttons
+  Widget cancelButton = FlatButton(
+    child: Text("Cancelar"),
+    onPressed:  () {
+      Navigator.of(context).pop();
+    },
+  );
+  Widget continueButton = FlatButton(
+    child: Text("Guardar"),
+    onPressed:  () {
+      _actualizar(nuevoValor,columna);
+      Navigator.of(context).pop();
+    },
+  );
 
-  var idPersona = localStorage.getInt('idPersona');
-  print(idPersona);
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Modificar dato"),
+    content: new Row(
+          children: <Widget>[
+            new Expanded(
+                child: new TextField(
+              autofocus: true,
+              decoration: new InputDecoration(
+                  labelText: subTitulo, hintText: valor),
+              onChanged: (valorIngresado) {
+                nuevoValor = valorIngresado;
+              },
+            ))
+          ],
+        ),
+        actions: [
+          cancelButton,
+          continueButton, 
+          ]   
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+}
+
+showAlertDialogMail(BuildContext context,valor,subTitulo,columna) {
+
+  // set up the buttons
+  Widget cancelButton = FlatButton(
+    child: Text("Cancelar"),
+    onPressed:  () {
+      Navigator.of(context).pop();
+    },
+  );
+  Widget continueButton = FlatButton(
+    child: Text("Guardar"),
+    onPressed:  () {
+      _actualizarMail(nuevoMail,clave);
+      Navigator.of(context).pop();
+    },
+  );
+
+  // set up the AlertDialog
+  AlertDialog alert = AlertDialog(
+    title: Text("Modificar Mail"),
+    content:new Column(
+      children: <Widget>[
+          new Row(
+                  children: <Widget>[
+                    new Expanded(
+                        child: new TextField(
+                        autofocus: true,
+                        decoration: new InputDecoration(
+                            labelText: subTitulo, hintText: valor),
+                        onChanged: (valorIngresado) {
+                          nuevoMail = valorIngresado;
+                        },
+                      )
+                    ),
+                  ]),
+          new Row(
+            children: <Widget>[
+              new Expanded(
+                  child: new TextField(
+                  autofocus: true,
+                  decoration: new InputDecoration(
+                      labelText:'Contraseña', hintText: ''),
+                  onChanged: (valorIngresado) {
+                    clave = valorIngresado;
+                  },
+                )
+              ),
+            ],
+          ),
+      ]
+    
+    ),
+        actions: [
+          cancelButton,
+          continueButton, 
+          ]   
+  );
+
+  // show the dialog
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return alert;
+    },
+  );
+
+
 }
