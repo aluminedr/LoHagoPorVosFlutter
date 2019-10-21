c008efb5ecfd672cbfcd3be6b8d7c2959a14395a
aluminedr <aluminede@gmail.com>
2019-10-21 16:31:25 -0300

    arreglandoAPI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/home.dart';
 import 'package:flutter_app/pages/crearPerfil.dart';
+import 'package:flutter_app/pages/listaHabilidades.dart';
 import 'package:flutter_app/pages/login.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -48,7 +49,7 @@ class _MyAppState extends State<LoHagoPorVos> {
       debugShowCheckedModeBanner: false,
       title: 'LO HAGO POR VOS',
       home: Scaffold(
-        body: (_isLoggedIn && _hasProfile) ? HomePage() : (_isLoggedIn && !_hasProfile) ? ProbarPage() : LoginPage(),
+        body: (_isLoggedIn && _hasProfile) ? HomePage() : (_isLoggedIn && !_hasProfile) ? ListaHabilidadesPage() : LoginPage(),
       ),
       
     );
