ad6430f7a5f904ace38f323e9241814841c220f2
diegonmontes <49873628+diegonmontes@users.noreply.github.com>
2019-10-21 10:21:10 -0300

    Funciona redirect a ipv4 mercadopago

--- lib/pages/webViewContainer.dart
+++ lib/pages/webViewContainer.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
-import 'package:webview_flutter/webview_flutter.dart';
+import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
+//import 'package:webview_flutter/webview_flutter.dart';
 class WebViewContainer extends StatefulWidget {
   final url;  
   WebViewContainer(this.url);  
@@ -8,18 +11,41 @@ class WebViewContainer extends StatefulWidget {
 }
 class _WebViewContainerState extends State<WebViewContainer> {
   var _url;
-  final _key = UniqueKey();  _WebViewContainerState(this._url);  @override
+  //final _key = UniqueKey(); 
+  _WebViewContainerState(this._url); 
+   void initState() {
+        super.initState();
+        final fWPlugin = new FlutterWebviewPlugin();
+        fWPlugin.onDestroy.listen((_) => exit(0));
+        fWPlugin.onUrlChanged.listen((String url) {
+          print("URL: " + url);
+          if (url.contains('localhost')){
+            fWPlugin.stopLoading();      
+            String ipv4;
+            String urlIPV4;
+            ipv4="http://192.168.1.36/";
+            var urlSinLocalHost=(url.split("http://localhost/").last);
+            urlIPV4= ipv4+urlSinLocalHost;
+            //print(urlIPV4);
+            fWPlugin.reloadUrl(urlIPV4);
+          } else {
+            print("no contiene");
+          }
+
+        });
+      }
+
+
+    @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(),
-        body: Column(
-          children: [
-            Expanded(
-                child: WebView(
-                    key: _key,
-                    javascriptMode: JavascriptMode.unrestricted,
-                    initialUrl: _url))
-          ],
-        ));
+    return new WebviewScaffold(
+          url:_url,
+          appBar: new AppBar(
+            title: new Text("Pago"),
+          ),
+          withLocalStorage: true,
+          hidden: true,
+        );
   }
+  
 }
