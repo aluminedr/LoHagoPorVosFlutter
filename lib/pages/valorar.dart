60088a0cd21a7c1e8e8c3cb4a3b5aaf039becfdc
diegonmontes <49873628+diegonmontes@users.noreply.github.com>
2019-11-07 17:28:57 -0300

    agregando valorar.dart

--- lib/pages/valorar.dart
+++ lib/pages/valorar.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:smooth_star_rating/smooth_star_rating.dart'; 
+import 'package:flutter_app/api/api.dart';
+import 'dart:convert';
+
+import '../main.dart';
 
 class Valorar extends StatefulWidget{
-  final index;
   final idPersonaLogeada;
-  Valorar(this.index, this.idPersonaLogeada);
+  final idTrabajo;
+  Valorar(this.idPersonaLogeada, this.idTrabajo);
 
   @override
   _ValorarState createState() => _ValorarState();
@@ -12,26 +16,87 @@ class Valorar extends StatefulWidget{
 
 class _ValorarState extends State<Valorar> {
 
-  @override
-  Widget build(BuildContext context) {
-    var rating;
-    return Scaffold(
-      body:
-        Container(
-          child: SmoothStarRating(
-                allowHalfRating: false,
-                onRatingChanged: (v) {
-                  rating = v;
-                  setState(() {});
-                },
-                starCount: 5,
-                rating: rating,
-            size: 40.0,
-            color: Colors.green,
-            borderColor: Colors.purple,
-            spacing:0.0
-          ),
-        ),
-    );
+  var rating = 0.0;
+
+   void initState(){
+    buscarDatosPostulacion();
+    super.initState();  
   }
+
+ @override
+ Widget build(BuildContext context) {
+    print(widget.idTrabajo);
+    return new Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: new Text('Valorar'),
+      ),
+        body: Container(
+          child: Column(children: <Widget>[
+            Center(
+            child: SmoothStarRating(
+              rating: rating,
+              size: 45,
+              starCount: 5,
+              onRatingChanged: (value) {
+                setState(() {
+                  rating = value;
+                });
+              },
+            )
+            ),
+            SizedBox(
+              width: double.infinity,
+                        child: RaisedButton(
+                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+                          color: Colors.purple,
+                          textColor: Colors.white,
+                          child:Text(
+                            "Valorar", 
+                            style: TextStyle(
+                            fontWeight: FontWeight.normal
+                          ),),
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 16.0,
+                            horizontal: 32.0,
+                          ),
+                          onPressed: () {
+                            enviarValoracion();
+                          },
+                        ),
+                      ),
+          ],)
+        )
+    );
+ }
+ void enviarValoracion () async{
+    var idPersonaLogeada = widget.idPersonaLogeada;
+    var idTrabajo = widget.idTrabajo;
+    var valor = rating;
+    var data = {
+            'idPersonaLogeada':idPersonaLogeada,
+            'idTrabajo':idTrabajo,
+            'valor':valor,
+            'flutter':true,
+    };
+    var res = await CallApi().postData(data, 'enviarValoracion');
+    var body = json.decode(res.body);
+    if (body['success']){
+      Navigator.push(
+      context,
+      new MaterialPageRoute(
+          builder: (context) => LoHagoPorVos()));
+      }
+ }
+
+ void buscarDatosPostulacion() async {
+    var idTrabajo = widget.idTrabajo;
+    var data = {
+      'idTrabajo':idTrabajo,
+    };
+
+    var res = await CallApi().postData(data, 'buscarDatosPostulacion');
+    var body = json.decode(res.body);
+    print(body);
+ }
 }
